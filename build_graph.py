4b5953e2021f985dc8c2ab510d7c581f2feb86d3
Emily Silcock <emilysilcock@gmail.com>
2022-08-12 09:53:10 +0100

    Reformat into functions

--- build_graph.py
+++ build_graph.py
@@ -305,7 +305,12 @@ def create_node_vectors(
     print("Featurized matrix sizes:", x.shape, y.shape, tx.shape, ty.shape, allx.shape, ally.shape)
 
     for dat in [x, y, tx, ty, allx, ally]:
-        string_name = f'{dat=}'.split('=')[0]
+
+        callers_local_vars = inspect.currentframe().f_back.f_locals.items()
+        string_name = [var_name for var_name, var_val in callers_local_vars if var_val is dat]
+
+        print(string_name)
+
         f = open(f"data/ind.{dataset}.{string_name}", 'wb')
         pkl.dump(dat, f)
         f.close()
