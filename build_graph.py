630b0c1b4d3a645b7f5937082e33315a5986452d
Emily Silcock <emilysilcock@gmail.com>
2022-08-12 10:24:54 +0100

    Reformat into functions

--- build_graph.py
+++ build_graph.py
@@ -193,8 +193,8 @@ def create_node_vectors(
     print("**** x ****")
     print(type(x))
 
-    for i in old_x.shape[0]:
-        for j in old_x.shape[1]:
+    for i in range(old_x.shape[0]):
+        for j in range(old_x.shape[1]):
             if old_x[i][j] != x[i][j]:
                 print(old_x[i][j], x[i][j])
 
