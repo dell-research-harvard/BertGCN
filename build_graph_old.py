d1dfd6db912845e07f91c3b14857adfb91fa1bfc
Emily Silcock <emilysilcock@gmail.com>
2022-08-15 16:08:56 +0100

    Exploring what code does

--- build_graph_old.py
+++ build_graph_old.py
@@ -528,6 +528,10 @@ data = [0.0] * (len(row))
 new_adj = sp.csr_matrix((data, (row, col)), shape=(node_size, node_size))
 
 print("*******")
+print(new_adj.toarray())
+print("*******")
+print(adj.toarray())
+print("*******")
 print((adj != new_adj).nnz)
 print("*******")
 
