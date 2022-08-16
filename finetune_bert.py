5b85b78840424684d4e67ffc94bb1d37667d65a6
Emily Silcock <emilysilcock@gmail.com>
2022-08-16 10:17:40 +0100

    Put into functions

--- finetune_bert.py
+++ finetune_bert.py
@@ -1,14 +1,20 @@
+import os
+import argparse
+import shutil
+import logging
+
+from sklearn.metrics import accuracy_score
+from model import BertClassifier
+
 import torch as th
 import torch.nn.functional as F
-from utils import *
 import torch.utils.data as Data
+from torch.optim import lr_scheduler
+
 from ignite.engine import Events, create_supervised_evaluator, create_supervised_trainer, Engine
 from ignite.metrics import Accuracy, Loss
-import os
-from sklearn.metrics import accuracy_score
-import argparse, shutil, logging
-from torch.optim import lr_scheduler
-from model import BertClassifier
+
+from utils import *
 
 
 def load_parameters():
@@ -100,8 +106,6 @@ def load_data(dataset, logger):
         'test': y[-count['test nodes']:]
     }
 
-    print(label_dict['train'])
-
     # load documents
     corpus_file = './data/corpus/'+dataset+'_shuffle.txt'
     with open(corpus_file, 'r') as f:
@@ -112,18 +116,18 @@ def load_data(dataset, logger):
     return text, count, label_dict
 
 
-def tokenize_data(text, count, label_dict):
+def tokenize_data(text, count, label_dict, model, max_length):
 
     # Tokenize documents
     def encode_input(text, tokenizer):
         input = tokenizer(text, max_length=max_length, truncation=True, padding=True, return_tensors='pt')
         return input.input_ids, input.attention_mask
 
-    input_ids, attention_mask = {}, {}
-
     input_ids_, attention_mask_ = encode_input(text, model.tokenizer)
 
     # create train/test/val datasets and dataloaders
+    input_ids, attention_mask = {}, {}
+
     input_ids['train'], input_ids['val'], input_ids['test'] = \
         input_ids_[:count['train nodes']], \
         input_ids_[count['train nodes']:count['train nodes']+count['val nodes']], \
@@ -139,85 +143,92 @@ def tokenize_data(text, count, label_dict):
         datasets[split] = Data.TensorDataset(input_ids[split], attention_mask[split], label_dict[split])
         loader[split] = Data.DataLoader(datasets[split], batch_size=batch_size, shuffle=True)
 
-    return datasets, loader
+    return loader
 
 
-if __name__ == '__main__':
+def train_step(engine, batch):
 
-    max_length, batch_size, nb_epochs, bert_lr, dataset, bert_init, ckpt_dir, args = load_parameters()
+    global model, optimizer
 
-    logger, cpu, gpu = set_up_logging(ckpt_dir, args)
+    model.train()
 
-    text, count_dict, label_dict = load_data(dataset, logger)
+    model = model.to(gpu)
 
-    model = BertClassifier(pretrained_model=bert_init, nb_class=count_dict['classes'])
+    optimizer.zero_grad()
 
-    datasets, loader = tokenize_data(text, count_dict, label_dict)
+    (input_ids, attention_mask, label) = [x.to(gpu) for x in batch]
 
-    print("got to here")
+    optimizer.zero_grad()
+
+    y_pred = model(input_ids, attention_mask)
+    y_true = label.type(th.long)
+
+    loss = F.cross_entropy(y_pred, y_true)
+    loss.backward()
+
+    optimizer.step()
+
+    train_loss = loss.item()
+
+    with th.no_grad():
+        y_true = y_true.detach().cpu()
+        y_pred = y_pred.argmax(axis=1).detach().cpu()
+        train_acc = accuracy_score(y_true, y_pred)
+
+    return train_loss, train_acc
+
+
+def test_step(engine, batch):
+
+    global model
+
+    with th.no_grad():
+
+        model.eval()
+        model = model.to(gpu)
+
+        (input_ids, attention_mask, label) = [x.to(gpu) for x in batch]
+
+        optimizer.zero_grad()
+
+        y_pred = model(input_ids, attention_mask)
+        y_true = label
+
+        return y_pred, y_true
+
+
+def train(data_loader, model, bert_lr, ckpt_dir, nb_epochs):
 
     # Training
     optimizer = th.optim.Adam(model.parameters(), lr=bert_lr)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[30], gamma=0.1)
 
-    def train_step(engine, batch):
-        global model, optimizer
-        model.train()
-        model = model.to(gpu)
-        optimizer.zero_grad()
-        (input_ids, attention_mask, label) = [x.to(gpu) for x in batch]
-        optimizer.zero_grad()
-        y_pred = model(input_ids, attention_mask)
-        y_true = label.type(th.long)
-        loss = F.cross_entropy(y_pred, y_true)
-        loss.backward()
-        optimizer.step()
-        train_loss = loss.item()
-        with th.no_grad():
-            y_true = y_true.detach().cpu()
-            y_pred = y_pred.argmax(axis=1).detach().cpu()
-            train_acc = accuracy_score(y_true, y_pred)
-        return train_loss, train_acc
-
-
     trainer = Engine(train_step)
 
-
-    def test_step(engine, batch):
-        global model
-        with th.no_grad():
-            model.eval()
-            model = model.to(gpu)
-            (input_ids, attention_mask, label) = [x.to(gpu) for x in batch]
-            optimizer.zero_grad()
-            y_pred = model(input_ids, attention_mask)
-            y_true = label
-            return y_pred, y_true
-
-
     evaluator = Engine(test_step)
-    metrics={
+
+    metrics = {
         'acc': Accuracy(),
         'nll': Loss(th.nn.CrossEntropyLoss())
     }
+
     for n, f in metrics.items():
         f.attach(evaluator, n)
 
-
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_training_results(trainer):
-        evaluator.run(loader['train'])
+        evaluator.run(data_loader['train'])
         metrics = evaluator.state.metrics
         train_acc, train_nll = metrics["acc"], metrics["nll"]
-        evaluator.run(loader['val'])
+        evaluator.run(data_loader['val'])
         metrics = evaluator.state.metrics
         val_acc, val_nll = metrics["acc"], metrics["nll"]
-        evaluator.run(loader['test'])
+        evaluator.run(data_loader['test'])
         metrics = evaluator.state.metrics
         test_acc, test_nll = metrics["acc"], metrics["nll"]
         logger.info(
             "\rEpoch: {}  Train acc: {:.4f} loss: {:.4f}  Val acc: {:.4f} loss: {:.4f}  Test acc: {:.4f} loss: {:.4f}"
-            .format(trainer.state.epoch, train_acc, train_nll, val_acc, val_nll, test_acc, test_nll)
+                .format(trainer.state.epoch, train_acc, train_nll, val_acc, val_nll, test_acc, test_nll)
         )
         if val_acc > log_training_results.best_val_acc:
             logger.info("New checkpoint")
@@ -236,4 +247,19 @@ if __name__ == '__main__':
         scheduler.step()
 
     log_training_results.best_val_acc = 0
-    trainer.run(loader['train'], max_epochs=nb_epochs)
+    trainer.run(data_loader['train'], max_epochs=nb_epochs)
+
+
+if __name__ == '__main__':
+
+    max_length, batch_size, nb_epochs, bert_lr, dataset, bert_init, ckpt_dir, args = load_parameters()
+
+    logger, cpu, gpu = set_up_logging(ckpt_dir, args)
+
+    text, count_dict, label_dict = load_data(dataset, logger)
+
+    model = BertClassifier(pretrained_model=bert_init, nb_class=count_dict['classes'])
+
+    data_loader = tokenize_data(text, count_dict, label_dict, model, max_length)
+
+    train(data_loader, model, bert_lr, ckpt_dir, nb_epochs)
